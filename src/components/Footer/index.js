3a91e1e142c34d160abf21620af0cb0423aaade6
Staci Genesi <sgenesi@hotmail.com>
2021-05-02 19:14:31 -0600

    updated footer and added contact section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,34 +1,49 @@
-import React from 'react';
-// import '../App.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
+import React from "react";
 
 function Footer() {
-    return <div className='icons-list'>
-        <a
-            className="faLinkedin app-link social-media-list"
-            href="https://www.linkedin.com/in/staci-genesi-013b0372/"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-        </a>
-        <a
-            className="faGithub app-link social-media-list"
-            href="https://github.com/sgenesi"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-        </a>
-        <a
-            className="faInstagram app-link social-media-list"
-            href="https://www.instagram.com/sc_genesi/"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
-    </div>
+    return (
+        <footer className="footer">
+            <div>
+                <a
+                    href="https://github.com/sgenesi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={require("../../assets/logos/github-logo.png")}
+                        alt="Github"
+                        className="logo"
+                    ></img>
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://www.linkedin.com/in/staci-genesi-013b0372/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={require("../../assets/logos/linkedin-logo.png")}
+                        alt="LinkedIn"
+                        className="logo"
+                    ></img>
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://www.instagram.com/sc_genesi/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={require("../../assets/logos/instagram-logo.png")}
+                        alt="Instagram"
+                        className="logo"
+                    ></img>
+                </a>
+            </div>
+        </footer>
+    );
 }
+
 export default Footer;
